2a2aa8d71e55c4e74a0d836bbde203c88ebfe779
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-06 15:54:38 +0700

    define basic flow index and show user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  { id: 1, name: 'John', email: 'john@example.com' },
+  { id: 2, name: 'Jane', email: 'jane@example.com' },
+  { id: 3, name: 'Bob', email: 'bob@example.com' }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users', users: users });
+});
+
+/* GET user from list. */
+router.get('/:user_id', function(req, res, next) {
+  var user = users.find(function(u) { return u.id === parseInt(req.params.user_id); });
+  if (!user) {
+    return next();
+  }
+  res.render('user', { title: user.name, user: user });
 });
 
 module.exports = router;
