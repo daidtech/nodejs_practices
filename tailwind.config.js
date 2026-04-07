236d3a512e5f80111b697ff7d98c7237578d9897
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-07 09:42:36 +0700

    add tailwind to component pug

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.pug', './routes/**/*.js', './app.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
