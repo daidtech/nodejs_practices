e439ca4d3ab6edfb4efe7221672fbe677b91d336
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-08 09:57:22 +0700

    feat: add active status to User model and enhance posts page with additional post details

--- models/User.js
+++ models/User.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       enum: ['user', 'admin', 'editor'],
       default: 'user'
+    },
+    active: {
+      type: Boolean,
+      default: false
     }
   },
   {
