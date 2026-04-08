6380206bbafce0217e929bc919c8db8a58aa7d82
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-08 10:31:01 +0700

    feat: add loginCount field to User model for tracking user logins

--- models/User.js
+++ models/User.js
@@ -26,6 +26,10 @@ const userSchema = new mongoose.Schema(
     active: {
       type: Boolean,
       default: false
+    },
+    loginCount: {
+      type: Number,
+      default: 0
     }
   },
   {
